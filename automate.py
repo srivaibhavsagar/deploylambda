f1a426b4ceb76dd951083d1014f68a4b1d5010d0
“Vaibhav <vaibhavsrivastava59@gmail.com>
2022-11-19 07:24:51 +0000

    lambda creation

--- automate.py
+++ automate.py
@@ -7,7 +7,7 @@
 
 def lambda_handler(event, context):
     # TODO implement
-    print('hello from VSCODE')
+    print('hello from VSCODE...')
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
